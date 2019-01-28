2f26d9807144e0cc53cdaeba8bb35026e608fceb
Emerson Siega <emerson.siega@ciss.com.br>
2019-01-28 00:58:28 -0200

    Docker compose with rabbitMQ, front and back-end

--- dispatcher-service/src/main/java/br/com/kepha/challenge/dispatcherservice/config/MongoDBConfig.java
+++ dispatcher-service/src/main/java/br/com/kepha/challenge/dispatcherservice/config/MongoDBConfig.java
@@ -2,11 +2,15 @@ package br.com.kepha.challenge.dispatcherservice.config;
 
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 
 @Configuration
 public class MongoDBConfig extends AbstractMongoClientConfiguration {
+
+	@Value("${mongodb.uri}")
+	private String mongodbURI;
 
 	@Override
 	protected String getDatabaseName() {
@@ -15,6 +19,6 @@ public class MongoDBConfig extends AbstractMongoClientConfiguration {
 
 	@Override
 	public MongoClient mongoClient() {
-		return MongoClients.create("mongodb://admin:Admin777@ds123400.mlab.com:23400/sales-service");
+		return MongoClients.create(mongodbURI);
 	}
 }
--- sales-service/src/main/java/br/com/kepha/challenge/salesservice/App.java
+++ sales-service/src/main/java/br/com/kepha/challenge/salesservice/App.java
@@ -6,13 +6,12 @@ import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication(scanBasePackages = "br.com.kepha.challenge",
-		exclude={MongoAutoConfiguration.class})
+		exclude = {MongoAutoConfiguration.class})
 @EnableMongoRepositories(basePackages = "br.com.kepha.challenge")
 public class App {
 
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
-
 	}
 
 }
